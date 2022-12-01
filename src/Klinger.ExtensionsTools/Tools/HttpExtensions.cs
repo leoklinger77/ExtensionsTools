6d5cda82c67de9cf9fd446746022d925bb265645
Leandro Klinger <klinger@klingersystem.com>
2022-11-30 22:43:28 -0300

    Correcoes

--- src/Klinger.ExtensionsTools/Tools/HttpExtensions.cs
+++ src/Klinger.ExtensionsTools/Tools/HttpExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net.Http.Json;
+using System.Text;
 using System.Text.Json;
 
 namespace Klinger.ExtensionsTools.Tools
@@ -27,5 +28,28 @@ namespace Klinger.ExtensionsTools.Tools
          */
         public static T DeserializeObject<T>(this string data) =>
             JsonSerializer.Deserialize<T>(data, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+
+        public static StringContent GetContext(this object data)
+            => new StringContent(data.SerializeObject(), Encoding.UTF8, "application/json");
+
+        public static StreamContent FileStreamContent(this FileStream file)
+        {
+            if (file == null) return null;
+
+            return new StreamContent(file);
+        }
+
+        public static StreamContent FileStreamContent(this Stream file)
+        {
+            if (file == null) return null;
+
+            return new StreamContent(file);
+        }
+
+        public static string GetFileName(this string path)
+        {
+            return string.IsNullOrEmpty(path) ? null : Path.GetFileName(path);
+        }
     }
 }
